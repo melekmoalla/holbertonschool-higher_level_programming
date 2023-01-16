c6785af3bbc05a6d7492340cdf322ecc392f8b95
melekmoalla <melekmoalla2468@gmail.com>
2023-01-16 23:50:06 +0100

    fix style

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(9):
     for j in range(i+1, 9+1):
-        if i != j and  i < 8:
+        if i != j and i < 8:
             print("{:d}".format(i)+format(j), end=", ")
         else:
             print("{:d}".format(i)+format(j), end="\n")
