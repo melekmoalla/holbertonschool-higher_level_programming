08bf886dd5ac92b42dc8d6f6dce67ba5f1ddabbd
melekmoalla <melekmoalla2468@gmail.com>
2023-01-16 17:10:08 +0100

    test

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str[0:5]
-print(str[0:66])
+str = str[39:66]+" python"
+print(str)
