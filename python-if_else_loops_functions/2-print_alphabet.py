e78e31480fb2ee8861cf88e87c90aad375d72f25
melekmoalla <melekmoalla2468@gmail.com>
2023-01-16 21:57:36 +0100

    correct

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("".join(chr(i) for i in range(ord('a'), ord('z')+1)), end="")
+for alpha_letters in range(ord('a'), ord('z')+1):
+    if alpha_letters == 'e' or alpha_letters == 'q':
+       continue
+    print("{:c}".format(alpha_letters), end="")
