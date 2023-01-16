6a452c8b8f846901b3c8e260ed8c910ef54d4681
melekmoalla <melekmoalla2468@gmail.com>
2023-01-16 14:05:07 +0100

    test code

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str="object-oriented programming with Python"
+str=str.replace("Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax", "object-oriented programming with Python")
 print(str)
