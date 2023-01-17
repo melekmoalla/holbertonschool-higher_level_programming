72fd994a7557f8836df72b4a84986bdce87e5fe1
melekmoalla <melekmoalla2468@gmail.com>
2023-01-17 12:19:05 +0100

    test

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    """
+    add add,sub,div,mul
+    """
+    from calculator_1 import add, sub, div, mul
+
+    a = 10
+    b = 5
+    result1 = add(a, b)
+    print("{} + {} = {}".format(a, b, result1))
+    result2 = sub(a, b)
+    print("{} + {} = {}".format(a, b, result2))
+    result4 = mul(a, b)
+    print("{} + {} = {}".format(a, b, result4))
+    result3 = div(a, b)
+    print("{} + {} = {}".format(a, b, result3))
