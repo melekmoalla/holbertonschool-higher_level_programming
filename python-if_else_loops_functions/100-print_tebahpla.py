cc3a5e0f806804ba70405efe7362ae39a4401049
melekmoalla <melekmoalla2468@gmail.com>
2023-01-18 11:45:38 +0100

    test

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+a=1
+for x in list(range(ord('z'), ord('a') - 1, -1)):
+    if(a==1):
+        print(chr(x),end="")
+        a=2
+    elif(a==2):
+        print(chr(ord(chr(x))-32),end="")
+        a=1
