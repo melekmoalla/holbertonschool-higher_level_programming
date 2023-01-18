b5f0c2c1eac9495e12f9726771605014f1f82c98
melekmoalla <melekmoalla2468@gmail.com>
2023-01-18 11:47:49 +0100

    test

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,9 @@
- #!/usr/bin/python3
-
-a=1
+#!/usr/bin/python3
+a = 1
 for x in list(range(ord('z'), ord('a') - 1, -1)):
-    if(a==1):
-        print(chr(x),end="")
-        a=2
-    elif(a==2):
-        print(chr(ord(chr(x))-32),end="")
-        a=1
+    if (a == 1):
+        print(chr(x), end="")
+        a = 2
+    elif (a == 2):
+        print(chr(ord(chr(x))-32), end="")
+        a = 1
