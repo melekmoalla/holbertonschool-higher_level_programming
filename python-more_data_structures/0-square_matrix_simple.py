ea802c77cc446f90a09723c0b3e2cd79684df0ab
melekmoalla <melekmoalla2468@gmail.com>
2023-01-24 00:10:27 +0100

    test code

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matr = [[0 for i in range(len(matrix))] for j in range(len(matrix[0]))]
+    matr = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
     for i in range(len(matrix)):
         for m in range(len(matr[i])):
             matr[i][m] = matrix[i][m]*matrix[i][m]
