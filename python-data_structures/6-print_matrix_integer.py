761f70565db1fff9fe0831a113558a2f109d0201
melekmoalla <melekmoalla2468@gmail.com>
2023-01-23 15:02:16 +0100

    test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" " if (element != row[-1]) else "\n")
-
-
+    if len(matrix) == 0:
+        print("melek")
+    else:
+        for row in matrix:
+            for element in row:
+                print("{:d}".format(element), end=" " if (
+                    element != row[-1]) else "\n")
