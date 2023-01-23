adb19026ad0f04cd64a76353233564d90308f8fb
melekmoalla <melekmoalla2468@gmail.com>
2023-01-23 10:50:40 +0100

    correct 2-replace

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if (idx < 0):
-        return (my_list[idx])
+        return (my_list)
     if (len(my_list)-1 >= idx):
             my_list[idx] = element
             return (my_list)
