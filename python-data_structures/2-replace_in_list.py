f9f6375e5d9279bdbca5cd255dada234f9dc35cf
melekmoalla <melekmoalla2468@gmail.com>
2023-01-23 10:48:44 +0100

    test 2-main.c

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if (idx < 0):
+        return (my_list[idx])
+    if (len(my_list)-1 >= idx):
+            my_list[idx] = element
+            return (my_list)
+    return (my_list)
