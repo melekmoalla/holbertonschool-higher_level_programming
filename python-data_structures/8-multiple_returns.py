31746bc147c73dcc2ca9ded3b5107e815fe39b2e
melekmoalla <melekmoalla2468@gmail.com>
2023-01-23 16:22:41 +0100

    test 8-main.c

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    a = None
+    b = 0
+    if (sentence == None):
+        return (b, a)
+    else:
+        a = sentence[0]
+        b = len(sentence)
+        return (b, a)
