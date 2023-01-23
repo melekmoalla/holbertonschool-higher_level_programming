e7776633233b6889752c3414b6e3822a0d6ecaca
melekmoalla <melekmoalla2468@gmail.com>
2023-01-23 17:42:40 +0100

    test

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if i(dx < 0) or (idx >= len(my_list)):
+    if (idx < 0) or (idx >= len(my_list)):
         return my_list
     else:
         my_list[idx:idx+1] = []
