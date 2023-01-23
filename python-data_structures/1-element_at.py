6b8c97743dc361d2dd5e7b87b77c0a1aa313867e
melekmoalla <melekmoalla2468@gmail.com>
2023-01-23 10:33:18 +0100

    test

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if (idx < 0):
         return (None)
-    if (len(my_list) >= idx):
+    if (len(my_list)-1 >= idx):
         return (my_list[idx])
     else:
         return (None)
