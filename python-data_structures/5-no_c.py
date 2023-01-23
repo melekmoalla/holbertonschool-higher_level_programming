db54e6d57459115f3546c54e049f8ae0af26d57a
melekmoalla <melekmoalla2468@gmail.com>
2023-01-23 13:22:46 +0100

    test main-5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    b = 0
     for i in range(len(my_string)):
         if ((ord(my_string[i]) == ord('c') or (ord(my_string[i]) == ord('C')))):
             a = my_string[:i] + my_string[i+1:]
-    return (a)
+            b = 1
+    if (b == 1):
+        return (a)
+    else:
+        return (my_string)
