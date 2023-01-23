03d82e74d2867bbbc31156ea2a9105081e30f7e2
melekmoalla <melekmoalla2468@gmail.com>
2023-01-23 13:13:24 +0100

    test remove caractere

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    for i in range(len(my_string)):
+        if ((ord(my_string[i]) == ord('c') or (ord(my_string[i]) == ord('C')))):
+            a = my_string[:i] + my_string[i+1:]
+    return (a)
