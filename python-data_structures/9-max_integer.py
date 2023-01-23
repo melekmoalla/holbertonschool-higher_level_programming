9d28db4c3ae88fd9c604cde7cce00f5d0f0c8757
melekmoalla <melekmoalla2468@gmail.com>
2023-01-23 16:40:18 +0100

    test corrct

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     a = 0
+    if (my_list == None):
+        return (None)
     for i in range(len(my_list)):
         if (my_list[i] > a):
-            a=my_list[i]
-    return(a)
+            a = my_list[i]
+    return (a)
