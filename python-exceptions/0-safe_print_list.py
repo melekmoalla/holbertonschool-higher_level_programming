7dc49c139ab2645c01a9d6a3cb17e57a5ff077df
melekmoalla <melekmoalla2468@gmail.com>
2023-01-25 00:07:10 +0100

    test

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-
-    p = 0
-    for i in my_list:
-        print(i, end="")
-        p += 1
-        if (p == x):
-            print()
-            return (i)
-    print()
-    return (i)
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            count += 1
+        print()
+    except:
+        print()
+    return count
