338976733afa69d07020d458fca0e7e1d26f64ef
melekmoalla <melekmoalla2468@gmail.com>
2023-01-24 21:01:25 +0100

    test

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_string):
-    sum = 0
-    if type(roman_string) != str:
-        return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10,
-                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for i in roman_string:
-        for a in roman_dict:
-            if (a == i):
-                sum += roman_dict[a]
-    return sum
+values = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+}
+
+
+def roman_to_int(str):
+
+    total = 0
+    i = 0
+
+    while (i < len(str)):
+        s1 = values[str[i]]
+        if (i+1 < len(str)):
+            s2 = values[str[i+1]]
+            if (s1 >= s2):
+                total = total + s1
+                i = i + 1
+            else:
+                total = total - s1
+                i = i + 1
+        else:
+            total = total + s1
+            i = i + 1
+    return total
