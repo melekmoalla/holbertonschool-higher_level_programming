7b6c4409b2e92b7a09ba1267720ae84342d7c204
melekmoalla <melekmoalla2468@gmail.com>
2023-01-24 16:07:19 +0100

    corrct some mistake

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     a = 0
+    b = ""
     if (a_dictionary == None):
         return None
     for key in a_dictionary:
         if (a_dictionary[key] > a):
             a = a_dictionary[key]
-    return (a)
+            b=key
+            
+    return (b)
