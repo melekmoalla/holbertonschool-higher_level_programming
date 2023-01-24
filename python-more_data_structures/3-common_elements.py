28ff1f4b6b4a6e4fdcfcea57380caf1867266090
melekmoalla <melekmoalla2468@gmail.com>
2023-01-24 09:47:44 +0100

    test

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    a = 0
     for i in set_1:
         for f in set_2:
             if (i == f):
-                return (i)
+                a = i
+    return (a)
