5a4276267206992572098eb2944c89634c84fb37
melekmoalla <melekmoalla2468@gmail.com>
2023-01-25 11:50:29 +0100

    test

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         my_list[i]
         try:
             int(my_list[i])
-            print(my_list[i], end='')
+            print("{:d}".format(my_list[i]), end='')
             count += 1
         except:
             print
