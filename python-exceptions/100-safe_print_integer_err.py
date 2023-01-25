189c1c9cfd31c0ea1a616fa34d952695212a35db
melekmoalla <melekmoalla2468@gmail.com>
2023-01-25 23:05:45 +0100

    test

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,18 @@ def safe_print_integer_err(value):
     import sys
     try:
         int(value)
-        print("{:d}".format(int(value)))
+        print("{:d}".format(value))
         return True
-    except Exception:
+    
+    except IndexError:
+        sys.stderr.write(
+            "Exception: Unknown format code 'd' for object of type str \n")
+        return  False
+    except ValueError:
         sys.stderr.write(
             "Exception: Unknown format code 'd' for object of type str \n")
         return False
+    except Exception:
+        sys.stderr.write(
+            "Exception: unsupported format string passed to set.__format__  \n")
+        return  False
