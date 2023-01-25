fdbd75d03a33e72a2570633ffb84de106fc50340
melekmoalla <melekmoalla2468@gmail.com>
2023-01-25 23:41:06 +0100

    test coorect

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ def safe_print_integer_err(value):
     import sys
     try:
         print("{:d}".format(value))
-    except:
-        print("Exception: Unknown format code 'd' for object of type '{}'".format(type(value).__name__), file=sys.stderr)
+        return True
+    except ValueError as e:
+        print(e)
         return False
-    return True
