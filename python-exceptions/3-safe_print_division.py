2a612643de5f74e9e1de35825c71461fdc484c98
melekmoalla <melekmoalla2468@gmail.com>
2023-01-25 12:23:16 +0100

    add execept

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,6 +3,8 @@ def safe_print_division(a, b):
     result = None
     try:
         result = int(a) / int(b)
+    except:
+        result = None
     finally:
         print("Inside result: {}".format(result))
         return (result)
