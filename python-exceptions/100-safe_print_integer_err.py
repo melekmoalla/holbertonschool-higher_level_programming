4ac9a40492ce8710abd91766cafe2552f2e3a2b5
melekmoalla <melekmoalla2468@gmail.com>
2023-01-26 00:51:27 +0100

    addd condition float

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -15,9 +15,14 @@ def safe_print_integer_err(value):
         return True
 
     except ValueError:
-        sys.stderr.write(
-            "Exception: Unknown format code 'd' for object of type 'str'\n")
-        return False
+        if float(value).is_integer() == False:
+            sys.stderr.write(
+                "Exception: Unknown format code 'd' for object of type 'float'\n")
+            return False
+        else:
+            sys.stderr.write(
+                "Exception: Unknown format code 'd' for object of type 'str'\n")
+            return False
     except TypeError:
         sys.stderr.write(
             "Exception: unsupported format string passed to set.__format__\n")
