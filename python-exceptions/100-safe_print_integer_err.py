9402a66f7555a6c64a1e7c7d0adae713638c147c
melekmoalla <melekmoalla2468@gmail.com>
2023-01-25 23:51:27 +0100

    test

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as e:
-        print(e)
+        print(e ,file=sys.stderr)
         return False
