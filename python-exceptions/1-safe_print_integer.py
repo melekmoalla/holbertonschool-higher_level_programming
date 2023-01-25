dd502c23c57d119b9cd5041db3c51561df16e148
melekmoalla <melekmoalla2468@gmail.com>
2023-01-25 10:37:24 +0100

    corrcet if empty

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
+        if not value:
+            raise ValueError('empty string')
         int(value)
         print("{:d}".format(value))
         return True
