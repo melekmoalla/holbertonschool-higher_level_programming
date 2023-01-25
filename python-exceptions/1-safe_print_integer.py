b39a7d9684e7140d81cd03fc19ae8c84b7e28477
melekmoalla <melekmoalla2468@gmail.com>
2023-01-25 10:46:33 +0100

    test modify

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if not value and value != 0:
-            raise ValueError
         int(value)
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except Exception:
         return False
