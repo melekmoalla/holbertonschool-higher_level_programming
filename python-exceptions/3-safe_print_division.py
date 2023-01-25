7653c5168886d9bb95540a6cbbf0f12ece0f14c6
melekmoalla <melekmoalla2468@gmail.com>
2023-01-25 12:22:12 +0100

    test

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = int(a) / int(b)
+    finally:
+        print("Inside result: {}".format(result))
+        return (result)
