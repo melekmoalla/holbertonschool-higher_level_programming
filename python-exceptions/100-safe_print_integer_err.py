ca91569f0d9db797f95ea2fa7bda6bbc85881aee
melekmoalla <melekmoalla2468@gmail.com>
2023-01-26 00:30:13 +0100

    test

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     import sys
+    if value == None:
+        sys.stderr.write(
+            "Exception: None Value is not allowed \n")
+        return False
     try:
         int(value)
         print("{:d}".format(value))
         return True
-    
+
     except IndexError:
         sys.stderr.write(
             "Exception: Unknown format code 'd' for object of type str \n")
-        return  False
+        return False
     except ValueError:
         sys.stderr.write(
             "Exception: Unknown format code 'd' for object of type 'str'\n")
         return False
     except TypeError:
         print("Cannot operate on None Value")
-        return  False
+        return False
     except Exception:
         sys.stderr.write(
             "Exception: unsupported format string passed to set.__format__\n")
-        return  False
+        return False
