037c79abb76f3c69b5c6bc1553b166b50e1a483b
melekmoalla <melekmoalla2468@gmail.com>
2023-01-26 17:41:19 +0100

    test another metrhod

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,29 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     import sys
-    a = "Exception: unsupported format string passed to NoneType.__format__"
-    b = "Exception: Unknown format code 'd' for object of type 'float'"
-    c = "Exception: Unknown format code 'd' for object of type 'str'"
-    if value is None:
-        print("{}".format(a), file=sys.stderr)
-        return False
-    if isinstance(value, list):
-        sys.stderr.write(
-            "Exception: unsupported format string passed to list.__format__\n")
-        return False
     try:
-        int(value)
-        print("{:d}".format(value))
+        print("{:d}".format((value)))
         return True
-
-    except ValueError:
-        if float(value).is_integer() is False:
-            print("{}".format(b), file=sys.stderr)
-            return False
-        else:
-            print("{}".format(c), file=sys.stderr)
-            return False
-    except TypeError:
-        sys.stderr.write(
-            "Exception: unsupported format string passed to set.__format__\n")
+    except Exception as i:
+        print("Exception: {}".format(i), file=sys.stderr)
         return False
