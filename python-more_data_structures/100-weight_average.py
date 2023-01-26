3b73e05fe65e83a3aaf8876e8d4ac58f2a1c37ef
melekmoalla <melekmoalla2468@gmail.com>
2023-01-26 11:35:29 +0100

    test 1200-weight

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    normalization = 0
+    val = 0
+    sum = 1
+    for value in ((my_list)):
+        sum = 1
+        for a in ((value)):
+            sum *= a
+        val += sum
+        normalization += a
+    return (val / normalization)
