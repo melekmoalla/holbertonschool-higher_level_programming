041d9fe07943711313d95bcaf10f3134f3e6f372
melekmoalla <melekmoalla2468@gmail.com>
2023-01-26 01:03:13 +0100

    test pip8

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
     import sys
-    if value == None:
-        sys.stderr.write(
-            "Exception: unsupported format string passed to NoneType.__format__\n")
+    a = "Exception: unsupported format string passed to NoneType.__format__"
+    b = "Exception: Unknown format code 'd' for object of type 'float'"
+    c = "Exception: Unknown format code 'd' for object of type 'str'"
+    if value is None:
+        print("{}".format(a), file=sys.stderr)
         return False
     if isinstance(value, list):
         sys.stderr.write(
@@ -15,13 +17,11 @@ def safe_print_integer_err(value):
         return True
 
     except ValueError:
-        if float(value).is_integer() == False:
-            sys.stderr.write(
-                "Exception: Unknown format code 'd' for object of type 'float'\n")
+        if float(value).is_integer() is False:
+            print("{}".format(b), file=sys.stderr)
             return False
         else:
-            sys.stderr.write(
-                "Exception: Unknown format code 'd' for object of type 'str'\n")
+            print("{}".format(c), file=sys.stderr)
             return False
     except TypeError:
         sys.stderr.write(
