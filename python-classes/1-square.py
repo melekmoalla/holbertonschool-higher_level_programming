2f3d4d6bb908ff8ae172b5309c9cf3ea808136fe
melekmoalla <melekmoalla2468@gmail.com>
2023-01-30 18:09:15 +0100

    test method 2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
+"""
+Defines a square object with a size.
+
+Attributes:
+    __size (int): The size of a side of the square.
+"""
 class Square:
-    """
-    Defines a square object with a size.
 
-    Attributes:
-        __size (int): The size of a side of the square.
     """
+    The constructor for Square class.
 
+    Parameters:
+        size (int): The size of a side of the square.
+
+    """
     def __init__(self, size: int):
-        """
-        The constructor for Square class.
-
-        Parameters:
-            size (int): The size of a side of the square.
-
-        """
         self.__size = size
