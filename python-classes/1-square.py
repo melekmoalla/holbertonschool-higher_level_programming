c118b6d7d00cf3c6f017ba6aa1adeb978f2a0619
melekmoalla <melekmoalla2468@gmail.com>
2023-01-30 17:54:25 +0100

    test 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+square.py:  which defines a squree class.
+"""
+
+
+class Square(object):
+    def __init__(self, size):
+        """Initializes the ize."""
+        self._Square__size = size
