3be139fbe03ad59e9dd52d557fe58aee56a1d2b0
melekmoalla <melekmoalla2468@gmail.com>
2023-01-30 18:49:32 +0100

    test 2-main

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a square object with a size.
+
+Attributes:
+    __size (int): The size of a side of the square.
+"""
+
+class Square:
+
+    """
+    The constructor for Square class.
+
+    Parameters:
+        size (int): The size of a side of the square.
+
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
