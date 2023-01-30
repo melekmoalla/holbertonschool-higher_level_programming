1183916bfd317059ff6155f3ead4de605e66e66e
melekmoalla <melekmoalla2468@gmail.com>
2023-01-30 17:22:17 +0100

    test delet add 3

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# -*- coding: UTF-8 -*-
 """
 circle.py: The circle module, which defines a Circle class.
 """
 
 class Square:
-    """A Circle instance models a circle with a radius"""
+    def Square():
+        pass
