016a64538166412c107ee73ff0e4687675570d05
melekmoalla <melekmoalla2468@gmail.com>
2023-01-31 20:32:22 +0100

    corrct mistake in the fist condition

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,7 +7,7 @@ def matrix_divided(matrix, div):
     b = len(matrix[i])
     if type(div) not in [int, float]:
         raise TypeError(
-            "div must be a numbers")
+            "div must be a number")
     if (div == 0):
         raise ZeroDivisionError(
             "division by zero")
