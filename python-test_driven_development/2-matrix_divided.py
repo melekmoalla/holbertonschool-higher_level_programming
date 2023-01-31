12f489a976a3da94d1372ddbf267371f095c9e12
melekmoalla <melekmoalla2468@gmail.com>
2023-01-31 20:24:17 +0100

    tets 2-main first

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    a = 0
+    m = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
+    i = 0
+    b = len(matrix[i])
+    if type(div) not in [int, float]:
+        raise TypeError(
+                "div must be a numbers")
+    if (div == 0):
+        raise ZeroDivisionError(
+                "division by zero")
+    for i in range(len(matrix)):
+        if (b != len(matrix[i])):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        for a in range(len(matrix[i])):
+            if type(matrix[i][a]) not in [int, float]:
+                raise TypeError(
+                        "Each row of the matrix must have the same size")
+            m[i][a] = round((matrix[i][a]/div), 2)
+    return (m)
