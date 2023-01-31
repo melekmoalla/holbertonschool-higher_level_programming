d2d80d360573074f9153c79c87ef2e10fe305aff
melekmoalla <melekmoalla2468@gmail.com>
2023-01-31 20:31:09 +0100

    test add and move two condition

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,18 +7,18 @@ def matrix_divided(matrix, div):
     b = len(matrix[i])
     if type(div) not in [int, float]:
         raise TypeError(
-                "div must be a numbers")
+            "div must be a numbers")
     if (div == 0):
         raise ZeroDivisionError(
-                "division by zero")
+            "division by zero")
     for i in range(len(matrix)):
         if (b != len(matrix[i])):
             raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats")
+                "Each row of the matrix must have the same size")
         b = len(matrix[i])
         for a in range(len(matrix[i])):
             if type(matrix[i][a]) not in [int, float]:
                 raise TypeError(
-                        "Each row of the matrix must have the same size")
+                    "matrix must be a matrix (list of lists) of integers/floats")
             m[i][a] = round((matrix[i][a]/div), 2)
     return (m)
