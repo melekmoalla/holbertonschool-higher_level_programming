00232dc9aef03108e7a272963eda4b1932f975de
melekmoalla <melekmoalla2468@gmail.com>
2023-01-31 20:38:20 +0100

    fix style and documentation

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
+"""
+    Function that divided element in the matrix bu=y div 2.
+    matrix and div must be integers or floats, otherwise raise a TypeError
+    exception with the message a must be an integer or b must be an integer
+    matrix and div must be first casted to integers if they are float
+    Returns an integer: matrix modify
+"""
+
 
 def matrix_divided(matrix, div):
+    """
+    Function that divided element in the matrix bu=y div 2.
+    matrix and div must be integers or floats, otherwise raise a TypeError
+    exception with the message a must be an integer or b must be an integer
+    matrix and div must be first casted to integers if they are float
+    Returns an integer: matrix modify
+    """
     a = 0
     m = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
     i = 0
     b = len(matrix[i])
+    v = "matrix must be a matrix (list of lists) of integers/floats"
     if type(div) not in [int, float]:
         raise TypeError(
             "div must be a number")
@@ -19,6 +35,6 @@ def matrix_divided(matrix, div):
         for a in range(len(matrix[i])):
             if type(matrix[i][a]) not in [int, float]:
                 raise TypeError(
-                    "matrix must be a matrix (list of lists) of integers/floats")
+                    v)
             m[i][a] = round((matrix[i][a]/div), 2)
     return (m)
