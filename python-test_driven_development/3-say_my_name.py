8d4206abc2fc2de0e9fbeb93eb280c2d4eccb809
melekmoalla <melekmoalla2468@gmail.com>
2023-02-01 00:24:50 +0100

    fix style in 3-say and add two condition in test/3-say

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+"""
+    Function that print the first name and last name.
+    first_name and last_name must be strings otherwise, raise a TypeError
+    exception with the message first_name must be a string or last_name must
+    be a string
+    Returns an integer: My name is "+first_name+" "+last_name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Function that print the first name and last name.
+    first_name and last_name must be strings otherwise, raise a TypeError
+    exception with the message first_name must be a string or last_name must
+    be a string
+    Returns an integer: My name is "+first_name+" "+last_name
+    """
     if type(first_name)not in [str]:
         raise TypeError("first_name must be a string")
     if type(last_name)not in [str]:
