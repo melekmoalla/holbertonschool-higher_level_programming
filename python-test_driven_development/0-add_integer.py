dc62855a50e721d596fb5932398a21602620aeeb
melekmoalla <melekmoalla2468@gmail.com>
2023-01-31 17:54:56 +0100

    style fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,6 +9,13 @@
 
 
 def add_integer(a, b=98):
+    """
+    Function that adds 2 integers.
+    a and b must be integers or floats, otherwise raise a TypeError
+    exception with the message a must be an integer or b must be an integer
+    a and b must be first casted to integers if they are float
+    Returns an integer: the addition of a and b
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
