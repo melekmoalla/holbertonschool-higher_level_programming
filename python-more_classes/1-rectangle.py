7d0496d8373ba5efd113dd156344218acecfebad
melekmoalla <melekmoalla2468@gmail.com>
2023-02-01 19:56:48 +0100

    fix document

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
+""""
+class Rectangle that defines a rectangle
+- property def width(self): to retrieve it
+- property setter def width(self, value): to set it:
+    * width must be an integer, otherwise raise a TypeError
+     exception with the message width must be an integer
+    * if width is less than 0, raise a ValueError exception
+     with the message width must be >= 0
 
+- property def height(self): to retrieve it
+- property setter def height(self, value): to set it:
+    * height must be an integer, otherwise raise a TypeError
+     exception with the message height must be an integer
+    * if height is less than 0, raise a ValueError exception
+     with the message height must be >= 0
+
+"""
 class Rectangle:
 
+    """
+    - Instantiation with optional width and height: 
+    def __init__(self, width=0, height=0):
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
