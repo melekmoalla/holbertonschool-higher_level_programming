78bae3b224522cf10c7f122f25790a6ed874360d
melekmoalla <melekmoalla2468@gmail.com>
2023-02-01 16:01:40 +0100

    test 3

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,3 +5,17 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_positive_integers(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_negative_integers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_mixed_integers(self):
+        self.assertEqual(max_integer([1, -2, 3, -4]), 3)
+
+if __name__ == '__main__':
+    unittest.main()
