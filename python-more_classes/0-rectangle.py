f8faf45caaeec456763e5decf2b296fe1c05a4b5
melekmoalla <melekmoalla2468@gmail.com>
2023-02-01 18:29:00 +0100

    fix documentation

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-
+"""
+an empty class Rectangle that defines a rectangle:
+"""
 class Rectangle:
+    """
+    an empty class Rectangle that defines a rectangle:
+    """
     pass
