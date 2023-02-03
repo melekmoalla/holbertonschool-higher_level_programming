d4aabdc7894f7448e1fc771c0e6cae2813a2e175
melekmoalla <melekmoalla2468@gmail.com>
2023-02-03 01:22:29 +0100

    corrct mistake

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -10,17 +10,4 @@ def lazy_matrix_mul(m_a, m_b):
     """Module to find the max integer in a list
     """
 
-    if (type(m_a)is not list):
-        raise TypeError("m_a must be a list or m_b must be a list")
-    if (type(m_b)is not list):
-        raise TypeError("m_b must be a list or m_b must be a list")
-    if ((type(i)is not list) for i in m_b):
-        raise TypeError("m_b must be a list or m_b must be a list")
-    if ((type(i)is not list) for i in m_a):
-        raise TypeError("m_a must be a list or m_b must be a list")
-    if m_a==[] or m_a == [[]]:
-        raise ValueError("m_a should contain only integers or floats")
-    if m_b==[] or m_b == [[]]:
-        raise ValueError("m_b should contain only integers or floats")
-
     return (np.dot(m_a, m_b))
