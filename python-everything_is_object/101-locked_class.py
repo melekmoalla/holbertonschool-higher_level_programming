f3b13478be49e09b9258884f1979ecfdd274ddd0
melekmoalla <melekmoalla2468@gmail.com>
2023-02-06 17:19:05 +0100

    add documantation

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""""
+    class LockedClass with no function or object
+"""""
 class LockedClass:
+    """""
+    creating new instance attributes
+    """""
     __slots__=["first_name"]
