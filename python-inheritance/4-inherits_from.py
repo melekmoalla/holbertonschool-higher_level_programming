e597cc06ce3f2e5a4214ac6e41df682672c168f4
melekmoalla <melekmoalla2468@gmail.com>
2023-02-07 00:02:29 +0100

    delet documentation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-Write a function that returns True if the object
-is an instance of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.
-"""
-
-
 def inherits_from(obj, a_class):
-    pass
+    """
+    Prototype: def inherits_from(obj, a_class)
+    """
+
+    if isinstance(obj, a_class):
+        return(True)
+    else:
+        return(False)
