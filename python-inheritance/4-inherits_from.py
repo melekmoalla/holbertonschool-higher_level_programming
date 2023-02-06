0affbc2955443c21e08c7243a018de24ba5d301e
melekmoalla <melekmoalla2468@gmail.com>
2023-02-07 00:05:17 +0100

    test

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """
 Write a function that returns True if the object
+is an instance of a class that inherited
 """
 
 
 def inherits_from(obj, a_class):
     """
-    Prototype: def inherits_from(obj, a_class)
+    Prototype: def inherits_from(obj, a_class):
     """
-
+    if (type(obj) is a_class):
+        return (False)
     if isinstance(obj, a_class):
         return(True)
     else:
