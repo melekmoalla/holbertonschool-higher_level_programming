ff0d4b783023aba1558a8b2354b6c09d5d1e5fac
melekmoalla <melekmoalla2468@gmail.com>
2023-02-06 22:28:42 +0100

    add documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
+class MyList that inherits from list
 """
 class MyList(list):
     """
+    Public instance method: def print_sorted(self):
+    that prints the list, but sorted (ascending sort)
     """
     def print_sorted(self):
         print(self)
