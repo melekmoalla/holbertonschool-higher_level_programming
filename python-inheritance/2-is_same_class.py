bf8616a9a680afdc6bef13029c6255eb5555f6d5
melekmoalla <melekmoalla2468@gmail.com>
2023-02-06 23:11:14 +0100

    test ' first

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,5 +5,8 @@ a function that returns True if the object is exactly
 Prototype: def is_same_class(obj, a_class):
 """
 def is_same_class(obj, a_class):
+    """
+    Prototype: def is_same_class(obj, a_class):
+    """
     if (type(obj) == a_class):
         return (True)
