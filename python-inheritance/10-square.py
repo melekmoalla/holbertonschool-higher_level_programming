0a60b816ae73a70d2cd260ccac6aca254d9f1423
melekmoalla <melekmoalla2468@gmail.com>
2023-02-07 16:51:07 +0100

    fix style

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 
+"""
+a class Square that inherits from Rectangle (9-rectangle.py):
+"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-
+    """
+    *Instantiation with size: def __init__(self, size)::
+    *size must be private. No getter or setter
+    *size must be a positive integer, validated by integer_validator
+    *the area() method must be implemented
+    """
     def __init__(self, size):
         super().__init__(size, size)
