5a50a7c8010b11930b414e5b5fce851ff08f0e9e
melekmoalla <melekmoalla2468@gmail.com>
2023-02-07 16:15:12 +0100

    it's very hard this option you czn't thing it very fast

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,38 +4,9 @@
 
 """
 
-
-class BaseGeometry:
-    """
-    * Public instance method: def area(self): that raises
-    an Exception with the message area() is not implemented
-    * def integer_validator(self, name, value):
-        - you can assume name is always a string
-        - if value is not an integer: raise a TypeError exception,
-    with the message <name> must be an integer
-       - if value is less or equal to 0: raise a ValueError
-    exception with the message <name> must be greater than 0
-    """
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """
-    * Instantiation with width and height:
-     def __init__(self, width, height):
-    * width and height must be private. No getter or setter
-    * width and height must be positive integers, validated
-    by integer_validator
-    """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
