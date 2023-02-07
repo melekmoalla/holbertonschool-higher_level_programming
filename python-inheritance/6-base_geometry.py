3eac54282d30f73713fdc6fcd6c0a05549f6acf4
melekmoalla <melekmoalla2468@gmail.com>
2023-02-07 11:13:16 +0100

    fix style

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,7 +6,7 @@ a class BaseGeometry
 
 class BaseGeometry:
     """
-    Public instance method: def area(self): that raises 
+    Public instance method: def area(self): that raises
     an Exception with the message area() is not implemented
     """
     def area(self):
