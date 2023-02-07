bade0093e9ba932bd913c1c8798b69c62182d166
melekmoalla <melekmoalla2468@gmail.com>
2023-02-07 16:17:33 +0100

    test corrct fi all

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,9 +4,17 @@
 
 """
 
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
+    """
+    Instantiation with width and height: def __init__(self, width, height):
+    * width and height must be private. No getter or setter
+    * width and height must be positive integers, validated
+     by integer_validator
+    """
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
