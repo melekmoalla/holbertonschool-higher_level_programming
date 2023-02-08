62803565a7f9e0cb7744d01a40621dff44757f89
melekmoalla <melekmoalla2468@gmail.com>
2023-02-08 18:23:22 +0100

    add documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+Write a function that reads a text file (UTF8)
+and prints it to stdout
+"""
+
 
 def read_file(filename=""):
+    """
+    * Prototype: def read_file(filename=""):
+    * You must use the with statement
+    * You don’t need to manage file permission or
+    file doesn't exist exceptions.
+    """
     with open(filename, encoding='utf-8') as name_files:
-        print(name_files.read(),end="")
+        print(name_files.read(), end="")
         name_files.close()
