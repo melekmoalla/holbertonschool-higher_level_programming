5e0def403e138078015ad4aaad13e04843033304
melekmoalla <melekmoalla2468@gmail.com>
2023-02-08 23:43:22 +0100

    fix style

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Write a function that writes an Object to a text file, using a JSON representation:
+Write a function that writes an Object to a text file,
+ using a JSON representation:
 """
 
 
