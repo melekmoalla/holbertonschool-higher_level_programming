2a8fc966c7de0059a72dba7d8452aa9de8f181a4
melekmoalla <melekmoalla2468@gmail.com>
2023-02-08 18:43:29 +0100

    test somthing

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -14,5 +14,5 @@ def write_file(filename="", text=""):
     * Your function should overwrite the content of the file
      if it already exists.
     """
-    with open(filename, "a", encoding='utf-8') as f:
+    with open(filename, "w", encoding='utf-8') as f:
         return (f.write(text))
