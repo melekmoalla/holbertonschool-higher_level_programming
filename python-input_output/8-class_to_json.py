ac888937ba927fc0b1acf987ff55e0573f9d2321
melekmoalla <melekmoalla2468@gmail.com>
2023-02-09 11:22:16 +0100

    test something to work

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,8 +4,6 @@ Write a function that returns the dictionary description
  with simple data structure (list, dictionary, string,
  integer and boolean) for JSON serialization of an object:
 """
-import json
-
 
 def class_to_json(obj):
     """
