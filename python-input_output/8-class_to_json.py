89c51a3511341c7ecb93302989c29d19b88e5bb7
melekmoalla <melekmoalla2468@gmail.com>
2023-02-09 11:21:00 +0100

    test class_to_json

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Write a function that returns the dictionary description
+ with simple data structure (list, dictionary, string,
+ integer and boolean) for JSON serialization of an object:
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    * Prototype: def class_to_json(obj):
+    * obj is an instance of a Class
+    * All attributes of the obj Class are serializable:
+     list, dictionary, string, integer and boolean
+    """
+    obj_dict = obj.__dict__
+    return (obj_dict)
