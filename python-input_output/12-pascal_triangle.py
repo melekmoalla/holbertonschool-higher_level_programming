84d7fe7c0bf8c34ec28d0895036fa7ed2cb3d15c
melekmoalla <melekmoalla2468@gmail.com>
2023-02-09 20:10:58 +0100

    test corrct

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -11,18 +11,18 @@ def pascal_triangle(n):
     * Returns an empty list if n <= 0
     * You can assume n will be always
     an integer
-    """
-    if (n < 0):
+    * Representing the pascal's triangle of n. """
+    if (n <= 0):
         return []
-    my_list = []
-    for line in range(1, n+1):
-        C = 1  # used to represent C(line, i)
-        mylist2 = []
-        for i in range(1, line + 1):
-            # The first value in a
-            # line is always 1
-            mylist2.append(C)
-            C = int(C * (line - i) / i)
-        my_list.append(mylist2)
 
-    return (my_list)
+    listt = []
+    for i in range(n):
+        if i == 0:
+            listt.append([1])
+        else:
+            row = [1]
+            for j in range(1, i):
+                row.append(listt[i-1][j-1] + listt[i-1][j])
+            row.append(1)
+            listt.append(row)
+    return listt
