690baa8c5abfa8786af84ce50453a33736d6a550
melekmoalla <melekmoalla2468@gmail.com>
2023-02-13 11:11:37 +0100

    test first

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Write the first class Base
+"""
+
+
+class Base:
+    """""
+    * Create a file named models/base.py:
+
+    * Class Base:
+    * private class attribute __nb_objects = 0
+    * class constructor: def __init__(self, id=None)::
+        - if id is not None, assign the public instance
+         attribute id with this argument value -
+          you can assume id is an integer and you
+           don’t need to test the type of it
+        otherwise, increment __nb_objects and
+        assign the new value to the public instance attribute id
+    """""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
