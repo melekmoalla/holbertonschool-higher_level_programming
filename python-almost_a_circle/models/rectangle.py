93a38866ba5dbba977b107ecf73bb4ef87d2a66d
melekmoalla <melekmoalla2468@gmail.com>
2023-02-14 12:41:46 +0100

    fix pep8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -176,4 +176,5 @@ class Rectangle(Base):
             - x
             - y
         """
-        return {'x': self.x, 'y': self.y, 'id': self.id, 'height': self.height, 'width': self.width}
+        return {'x': self.x, 'y': self.y,
+                'id': self.id, 'height': self.height, 'width': self.width}
