94bb3ad031782b25013967ce50107a51c8b75e7c
melekmoalla <melekmoalla2468@gmail.com>
2023-02-14 17:31:56 +0100

    test first adding the class method def create(cls, **dictionary)

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -79,5 +79,25 @@ class Base:
             -Otherwise, return the list represented by json_string
         """
         if (json_string is None):
-            return([])
+            return ([])
         return (json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        *vadding the class method def create(cls, **dictionary):
+        that returns an instance with all attributes already set:
+
+        **dictionary can be thought of as a double pointer to a dictionary
+        -   To use the update method to assign all attributes, you must
+        create a “dummy” instance before:
+        -   Create a Rectangle or Square instance with “dummy” mandatory
+         attributes (width, height, size, etc.)
+        -   Call update instance method to this “dummy” instance to
+        apply your real values
+        -   You must use the method def update(self, *args, **kwargs)
+        -   **dictionary must be used as **kwargs of the method update
+        -   You are not allowed to use eval
+        """
+        cls.update(None)
+        return (cls(**dictionary))
