8264d97431c30c27cfcfc8f5464b2cb7930511ed
melekmoalla <melekmoalla2468@gmail.com>
2023-02-16 00:13:07 +0100

    test first rectangle

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestRectangle(unittest.TestCase):
+
+    def test_new_Rectangle(self):
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+
+        r3 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r3.id, 2)
+
+        r4 = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(r4.id, 5)
+
+        r5 = Rectangle("1", 2)
+        self.assertEqual(r5.id, 3)
+
+        r6 = Rectangle(1, "2")
+        self.assertEqual(r6.id, 4)
+
+        r7 = Rectangle(1, 2, "3")
+        self.assertEqual(r7.x, '3')
+
+        r8 = Rectangle(1, 2, 3, "4")
+        self.assertEqual(r8.y, '4')
+
+        r9 = Rectangle(-1, 2)
+        self.assertEqual(r9.width, -1)
