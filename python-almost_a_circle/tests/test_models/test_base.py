df990ef611486a08ab29299da95093ba6d23fdce
melekmoalla <melekmoalla2468@gmail.com>
2023-02-15 23:34:24 +0100

    first test after working hhh pis

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -15,3 +15,17 @@ class TestBase(unittest.TestCase):
     def test_specified_id(self):
         b3 = Base(89)
         self.assertEqual(b3.id, 89)
+
+    def test_to_json_string_none(self):
+        rjson = Base.to_json_string(None)
+        self.assertEqual(rjson, '[]')
+
+        json_dictionary = Base.to_json_string([])
+        self.assertEqual(json_dictionary, '[]')
+
+        json_dictionary = Base.to_json_string([{'id': 12}])
+        self.assertEqual(json_dictionary, '[{"id": 12}]')
+
+    def test_from_json_string(self):
+        list_output = Base.from_json_string(None)
+        self.assertEqual(list_output, [])
