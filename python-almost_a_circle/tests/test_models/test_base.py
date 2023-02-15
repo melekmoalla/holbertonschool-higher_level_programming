abcf4d1b39e68df8d576f7339d742f3031c1d62f
melekmoalla <melekmoalla2468@gmail.com>
2023-02-15 16:55:25 +0100

    test file test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,20 +1,23 @@
-#!/usr/bin/python3
+# tests/test_base.py
 
 import unittest
 from models.base import Base
 
+
 class TestBase(unittest.TestCase):
 
-
-    def test_base_id(self):
-
+    def test_new_instance_id(self):
         b1 = Base()
+        self.assertEqual(b1.id, 1)
         b2 = Base()
-        self.assertEqual(b1.id + 1, b2.id)
+        self.assertEqual(b2.id, 2)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_specified_id(self):
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
 
-
-
-
+    def test_multiple_specified_ids(self):
+        b4 = Base(12)
+        self.assertEqual(b4.id, 12)
+        b5 = Base(13)
+        self.assertEqual(b5.id, 13)
