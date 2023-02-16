3e5dde5a01d32889a7df2e73046c20965c85319c
melekmoalla <melekmoalla2468@gmail.com>
2023-02-16 13:39:40 +0100

    test Rectangle.save_to_file([])

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -105,13 +105,13 @@ class TestRectangle(unittest.TestCase):
 
     def test_save_to_file(self):
 
-        Rectangle.save_to_file(None)
-        with open("Rectangle.json", "r") as file:
-            self.assertEqual(file.read(), '[]')
-
         Rectangle.save_to_file([])
         with open("Rectangle.json", "r") as fil:
             self.assertEqual(fil.read(), '[]')
+            
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual(file.read(), '[]')
 
         Rectangle.save_to_file([Rectangle(1, 2)])
         with open("Rectangle.json", "r") as f:
