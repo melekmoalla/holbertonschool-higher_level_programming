483a110c22d06db6ca34a43091956c1077ed7c5a
melekmoalla <melekmoalla2468@gmail.com>
2023-02-16 22:56:47 +0100

    test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -144,23 +144,14 @@ class TestRectangle(unittest.TestCase):
         r = Rectangle(2, 4, 1, 1, 10)
         self.assertEqual(str(r), "[Rectangle] (10) 1/1 - 2/4")
 
-    def test_display(self):
-        with self.assertRaises(TypeError) as e:
-            r = Rectangle()
-            r.display(12, 12)
-        self.assertEqual(str(e.exception), str(e.exception))
-
     def test_display_exit(self):
         r1 = Rectangle(4, 6)
         self.assertEqual(r1.display(), None)
 
         r1 = Rectangle(4, 6)
         r1.display()
-        self.assertEqual(str(r1), "[Rectangle] (5) 0/0 - 4/6")
+        self.assertEqual(str(r1), "[Rectangle] (6) 0/0 - 4/6")
 
-        r = Rectangle(2, 3)
-        r.display()
-        self.assertEqual(r.display(), None)
 
 
 if __name__ == '__main__':
