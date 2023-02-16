e7b524ece854659db096c72d413cd10795b17485
melekmoalla <melekmoalla2468@gmail.com>
2023-02-16 23:53:55 +0100

    test 17

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -119,7 +119,7 @@ class TestRectangle(unittest.TestCase):
         Rectangle.save_to_file([Rectangle(1, 2)])
         with open("Rectangle.json", "r") as f:
             self.assertEqual(
-                f.read(), '[{"x": 0, "y": 0, "id": 13, "height": 2, "width": 1}]')
+                f.read(), '[{"x": 0, "y": 0, "id": 14, "height": 2, "width": 1}]')
 
     def test_load_from_to_life(self):
         r61 = Rectangle(10, 7, 2, 8)
@@ -138,6 +138,11 @@ class TestRectangle(unittest.TestCase):
     def test_str(self):
         r = Rectangle(2, 4, 1, 1, 10)
         self.assertEqual(str(r), "[Rectangle] (10) 1/1 - 2/4")
+
+    def test_display_test(self):
+        r200 = Rectangle(2, 3, 2, 2)
+        m = r200.display()
+        self.assertEqual(m, None)
 
     def test_display_exist(self):
         r1 = Rectangle(4, 6)
