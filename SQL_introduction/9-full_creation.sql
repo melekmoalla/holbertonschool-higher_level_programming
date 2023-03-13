d17546ddcad781cd3b828985ead5e20fbb49a5d8
melekmoalla <melekmoalla2468@gmail.com>
2023-03-13 16:47:54 +0100

    test 2

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,10 +6,10 @@ score INT
 );
 
 INSERT INTO second_table(id ,name,score)
-VALUES ('1', 'John', '10');
-VALUES ('2', 'Alex', '3');
-VALUES ('3', 'Bob', '14');
-VALUES ('4', 'George', '8');
+VALUES ('1', 'John', '10'),
+('2', 'Alex', '3'),
+('3', 'Bob', '14'),
+('4', 'George', '8');
 
 
 
