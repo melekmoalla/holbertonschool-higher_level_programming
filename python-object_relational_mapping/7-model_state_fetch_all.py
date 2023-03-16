04e4ba1c9e177bc0c86284c58cdf4b688627be6e
melekmoalla <melekmoalla2468@gmail.com>
2023-03-16 23:24:33 +0100

    test

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-mmm
+list list 
 """
+
 from model_state import Base, State
 from sqlalchemy import create_engine
 
 import MySQLdb
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     username, password, database = sys.argv[1:]
 
@@ -16,7 +17,7 @@ if __name__ == '__main__':
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
 
     Base.metadata.create_all(engine)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
