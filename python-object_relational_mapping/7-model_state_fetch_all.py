87083a64b5d913dffc76710de4987973e1b1dfd3
melekmoalla <melekmoalla2468@gmail.com>
2023-03-16 23:25:36 +0100

    test

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,15 +2,19 @@
 """
 list list 
 """
-
+# mmmm
 from model_state import Base, State
 from sqlalchemy import create_engine
 
 import MySQLdb
 import sys
-
+"""
+list list 
+"""
 if __name__ == "__main__":
-
+    """
+    list list 
+    """
     username, password, database = sys.argv[1:]
 
     engine = create_engine(
