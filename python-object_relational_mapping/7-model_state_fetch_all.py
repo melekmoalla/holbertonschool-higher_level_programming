d13c2e16b3f094090c52c499fc975f923dec4b2f
melekmoalla <melekmoalla2468@gmail.com>
2023-03-16 23:07:55 +0100

    test

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,39 +1,26 @@
 #!/usr/bin/python3
+
+
+"""
+Write a script that lists all State objects from the database hbtn_0e_6_usa
+
+Your script should take 3 arguments: mysql username,
+mysql password and database name
+You must use the module SQLAlchemy
+You must import State and Base from model_state -
+from model_state import Base, State
+Your script should connect to a MySQL server running on localhost at port 3306
+Results must be sorted in ascending order by states.id
+The results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
 from model_state import Base, State
 import sqlalchemy
 import MySQLdb
 import sys
 
-"""
-Write a script that lists all State objects from the database hbtn_0e_6_usa
+if __name__ == '__main__':
 
-Your script should take 3 arguments: mysql username,
-mysql password and database name
-You must use the module SQLAlchemy
-You must import State and Base from model_state -
-from model_state import Base, State
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
-The results must be displayed as they are in the example below
-Your code should not be executed when imported
-"""
-
-
-if __name__ == "__main__":
-
-    """
-Write a script that lists all State objects from the database hbtn_0e_6_usa
-
-Your script should take 3 arguments: mysql username,
-mysql password and database name
-You must use the module SQLAlchemy
-You must import State and Base from model_state -
-from model_state import Base, State
-Your script should connect to a MySQL server running on localhost at port 3306
-Results must be sorted in ascending order by states.id
-The results must be displayed as they are in the example below
-Your code should not be executed when imported
-"""
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
