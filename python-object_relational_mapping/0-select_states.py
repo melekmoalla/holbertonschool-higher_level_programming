86a2b7fdaaa8ce6f1adabc91e3a8064126f59fbb
melekmoalla <melekmoalla2468@gmail.com>
2023-03-16 12:44:37 +0100

    test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,18 +13,16 @@ Your code should not be executed when imported
 """
 import MySQLdb
 
-if __name__ == "__main__":
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user='root',
+    passwd='root',
+    db='hbtn_0e_0_usa')
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user='root',
-        passwd='root',
-        db='hbtn_0e_0_usa')
+cur = db.cursor()
 
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    records = cur.fetchall()
-    for i in records:
-        print(i)
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+records = cur.fetchall()
+for i in records:
+    print(i)
