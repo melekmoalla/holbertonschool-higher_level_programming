419f42d89ea3b39378f4fea02d25894c4e01b5b0
melekmoalla <melekmoalla2468@gmail.com>
2023-03-17 15:01:34 +0100

    test

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
+"""
+ Write a script that deletes all State objects with a
+ name containing the letter a from the database hbtn_0e_6_usa
+"""
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import sys
 
 
-"""
- Write a script that deletes all State objects with a
- name containing the letter a from the database hbtn_0e_6_usa
-"""
+
 Base = declarative_base()
 class state(Base):
     """
