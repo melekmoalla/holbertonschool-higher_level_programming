ebd4af5bd6cf0cf8a865788def83ce8f4add96ed
melekmoalla <melekmoalla2468@gmail.com>
2023-03-17 13:12:46 +0100

    fix problem hhh

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""
-Write a script that lists all State objects
-from the database hbtn_0e_6_usa
-"""
+"""Lists all states from the database hbtn_0e_0_usa"""
 import sqlalchemy
 import MySQLdb
 import sys
-
 if __name__ == "__main__":
-
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -19,10 +14,9 @@ if __name__ == "__main__":
         user=username,
         passwd=password,
         db=database)
-
     cur = db.cursor()
-    cur.execute("INSERT INTO states (name) VALUES ('Louisiana')")
+    cur.execute("INSERT INTO states (id,name) VALUES (6,'Louisiana')")
+    db.commit()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-
     result = cur.fetchall()
     print(result[-1][0])
