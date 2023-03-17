8781f6e2b5fe6fdac8325b1004315629aeb58e00
melekmoalla <melekmoalla2468@gmail.com>
2023-03-17 15:00:22 +0100

    test

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,6 +11,10 @@ import sys
 """
 Base = declarative_base()
 class state(Base):
+    """
+    Write a script that deletes all State objects with a
+    name containing the letter a from the database hbtn_0e_6_usa
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
