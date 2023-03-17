287b1d5aca0bd388bea563f5e6d4a33c0034d366
melekmoalla <melekmoalla2468@gmail.com>
2023-03-17 17:43:15 +0100

    tets

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(user, password, db_name))
+                           .format(user, password, db_name), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
