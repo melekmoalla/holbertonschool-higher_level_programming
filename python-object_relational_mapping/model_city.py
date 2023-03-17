8c801b7e89ac52c54f060e17673a3398fae94d4c
melekmoalla <melekmoalla2468@gmail.com>
2023-03-17 18:38:02 +0100

    test

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -10,6 +10,10 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
+    """
+    Next, write a script 14-model_city_fetch_by_state.py that
+    prints all City objects from the database hbtn_0e_14_usa:
+    """
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
