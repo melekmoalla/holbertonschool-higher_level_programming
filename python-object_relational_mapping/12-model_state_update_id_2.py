d4cb4a167db9ae40112b390f9069d16774b39b2d
melekmoalla <melekmoalla2468@gmail.com>
2023-03-17 13:26:53 +0100

    Write a script that changes the name of a State object from the database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+import sqlalchemy
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database)
+    cur = db.cursor()
+    cur.execute("UPDATE states SET name = 'New Mexico' WHERE states.id = 2")
+    db.commit()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    result = cur.fetchall()
+    print(result[-1][0])
