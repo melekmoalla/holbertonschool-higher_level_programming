9d1d9928d33ff235f0a74f64dc19d1f1e65f96fd
melekmoalla <melekmoalla2468@gmail.com>
2023-03-17 17:39:32 +0100

    tets

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State):
+    for state in session.query(State).order_by(State.id):
         if 'a' in state.name:
-           session.delete(state)
+            session.delete(state)
 
     session.commit()
