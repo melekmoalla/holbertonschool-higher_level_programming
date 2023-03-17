d0999e80f128fa84df124bf944e0250dba490756
melekmoalla <melekmoalla2468@gmail.com>
2023-03-17 17:01:12 +0100

    tets

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -3,18 +3,19 @@
 deletes all State objects from the database that contain 'a'
 using SQLAlchemy and importing State and Base from model_state
 """
-from sys import argv
+import sys
 from model_state import Base, State
-
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-for state in session.query(State).filter(State.name.like('%a%')).all():
-    session.delete(state)     
-    
-session.commit()
+    for state in session.query(State).filter(State.name.like('%a%')).all():
+        session.delete(state)
+
+    session.commit()
