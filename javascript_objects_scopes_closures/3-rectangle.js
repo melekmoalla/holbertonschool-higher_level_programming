491f961ec0e00d5f4b45389dc83143ccf8ecb449
melekmoalla <melekmoalla2468@gmail.com>
2023-04-20 15:09:07 +0100

    test corrcet

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -4,14 +4,14 @@ class Rectangle {
     if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
       // Empty object
     } else {
-      this.weight = weight;
+      this.wight = weight;
       this.height = height;
     }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      for (let a = 0; a < this.weight; a++) {
+      for (let a = 0; a < this.wight; a++) {
         process.stdout.write('X');
       }
       process.stdout.write('\n');
