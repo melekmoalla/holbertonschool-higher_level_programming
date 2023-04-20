7817cbc0f815c99fc442b9749f0dbd2b676391b4
melekmoalla <melekmoalla2468@gmail.com>
2023-04-20 01:24:44 +0100

    fix semis

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 
 if (args[2] === undefined) {
-    console.log('No argument');}
-else {
-    console.log(args[2])
+  console.log('No argument');
+} else {
+  console.log(args[2]);
 }
