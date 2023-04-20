7c0b71a40665398545691bde453bb96d6a430f7f
melekmoalla <melekmoalla2468@gmail.com>
2023-04-20 11:45:56 +0100

    correct

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 function factorial (x) {
-  if (x === 0) {
+
+  if (isNaN(x)|| x === 0) {
     return 1;
   }
   return x * factorial(x - 1);
 }
 const arg1 = process.argv[2];
+
 console.log(factorial(arg1));
