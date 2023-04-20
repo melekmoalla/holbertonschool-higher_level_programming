bcb575f47d76326b12b948bd03d681adfcce94a4
melekmoalla <melekmoalla2468@gmail.com>
2023-04-20 12:33:01 +0100

    test

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const a = process.argv;
 let p = 0;
-let m = 0;
 for (let i = 2; i < a.length; i++) {
   if (p < parseInt(a[i])) {
-    m = p;
     p = a[i];
   }
 }
-console.log(m);
+console.log(p -1);
