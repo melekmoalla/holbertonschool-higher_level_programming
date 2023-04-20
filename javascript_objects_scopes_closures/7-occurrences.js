4888c161a5ce22b3d3771011fbd84b07cde84fe8
melekmoalla <melekmoalla2468@gmail.com>
2023-04-20 16:36:02 +0100

    correct

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,11 @@
 exports.nbOccurences = function (list, searchElement){
     const counts = {};
     for (const num of list) {
-        counts[num] = counts[num] ? counts[num] + 1 : 1;
+        if (Number.isInteger(searchElement) === true && Number.isInteger(num) === true){
+            counts[num] = counts[num] ? counts[num] + 1 : 1;
+        }
+        else{
+        counts[num] = counts[num] ? counts[num] + 1 : 1;}
       }
     if (counts[searchElement] === undefined){
         return 0
