4d26a3d8986e1e6eeaf4566bf41e0cfc9059ab82
melekmoalla <melekmoalla2468@gmail.com>
2023-04-24 01:08:25 +0100

    test fix semi

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-file = process.argv[2]
+const file = process.argv[2];
 
 const fs = require('fs');
 fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-  });
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
