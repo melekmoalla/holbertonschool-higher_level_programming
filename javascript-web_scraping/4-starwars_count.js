ec64f7bc9f267a09e88c72de71c239734b9943ab
melekmoalla <melekmoalla2468@gmail.com>
2023-04-24 17:08:19 +0100

    test file 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,14 +3,19 @@
 const movie = process.argv[2];
 
 const request = require('request');
-const m1 = movie.split('/');
-const m = `https://${m1[2]}/api/people/18`;
-request(m, (error, response, body) => {
+
+request(movie, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const movie5 = JSON.parse(body);
-    const len = movie5.films;
-    console.log(len.length);
+    const movie5 = JSON.parse(body).results;
+    let coun = 0;
+    for (let i = 0; i < movie5.length; i++) {
+      const p = movie5[i].characters;
+      if (p.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        coun++;
+      }
+    }
+    console.log(coun);
   }
 });
