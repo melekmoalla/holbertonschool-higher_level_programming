9fb92e9d77d5fa12f16ac4f3a9376d0e2336e77f
melekmoalla <melekmoalla2468@gmail.com>
2023-04-24 18:37:12 +0100

    xorking never give up

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,25 @@
 const movie = process.argv[2];
 
 const request = require('request');
-const m1 = movie.split('/');
-const m = `${m1[0]}//${m1[2]}/api/people/18`;
-request(m, (error, response, body) => {
+let m2;
+request(movie, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const movie5 = JSON.parse(body);
-    const len = movie5.films;
-    console.log(len.length);
+    const m1 = movie.split('/');
+    if (m1[2] === `http://swapi.co/api/people/18/`) {
+      m2 = `http://swapi.co/api/people/18/`;
+    } else {
+      m2 = `https://swapi-api.hbtn.io/api/people/18/`;
+    }
+    const movie5 = JSON.parse(body).results;
+    let coun = 0;
+    for (let i = 0; i < movie5.length; i++) {
+      const p = movie5[i].characters;
+      if (p.includes(m2)) {
+        coun++;
+      }
+    }
+    console.log(coun);
   }
 });
