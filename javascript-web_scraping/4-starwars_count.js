61c032f076dfb2cbd106e8d694c28b53abe71d18
melekmoalla <melekmoalla2468@gmail.com>
2023-04-24 17:40:41 +0100

    now is corrcte

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,18 @@
 #!/usr/bin/node
 
+
 const movie = process.argv[2];
 
 const request = require('request');
 const m1 = movie.split('/');
+console.log(m1)
 const m = `https://${m1[2]}/api/people/18/`;
-request(movie, (error, response, body) => {
+request(m, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    const movie5 = JSON.parse(body).results;
-    let coun = 0;
-    for (let i = 0; i < movie5.length; i++) {
-      const p = movie5[i].characters;
-      if (p.includes(m)) {
-        coun++;
-      }
-    }
-    console.log(coun);
+    const movie5 = JSON.parse(body);
+    const len = movie5.films;
+    console.log(len.length);
   }
 });
