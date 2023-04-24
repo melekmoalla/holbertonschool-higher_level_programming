cc638f7c621990474c70369f792b0cbdea2151bd
melekmoalla <melekmoalla2468@gmail.com>
2023-04-24 17:58:48 +0100

    test 404

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
-
 const movie = process.argv[2];
 
-if (movie !== undefined) {
-  const request = require('request');
-  const m1 = movie.split('/');
-  const m = `https://${m1[2]}/api/people/18`;
-  request(m, (error, response, body) => {
-    if (error) {
-      console.error(error);
-    } else {
-      const movie5 = JSON.parse(body);
-      const len = movie5.films;
-      console.log(len.length);
+const request = require('request');
+
+request(movie, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie5 = JSON.parse(body).results;
+    let coun = 0;
+    for (let i = 0; i < movie5.length; i++) {
+      const p = movie5[i].characters;
+      if (p.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        coun++;
+      }
     }
-  });
-}
+    console.log(coun);
+  }
+});
