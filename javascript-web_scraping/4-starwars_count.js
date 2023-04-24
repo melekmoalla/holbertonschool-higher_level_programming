3f12f352643dd4808bc09b88366483e5a51aadf4
melekmoalla <melekmoalla2468@gmail.com>
2023-04-24 18:02:42 +0100

    test 404

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,6 +4,8 @@ const movie = process.argv[2];
 const request = require('request');
 
 request(movie, (error, response, body) => {
+    const m1 = movie.split('/');
+    const m = `https://${m1[2]}/api/people/18/`;
   if (error) {
     console.error(error);
   } else {
@@ -11,7 +13,7 @@ request(movie, (error, response, body) => {
     let coun = 0;
     for (let i = 0; i < movie5.length; i++) {
       const p = movie5[i].characters;
-      if (p.includes('http://swapi.co/api/people/18/')) {
+      if (p.includes(m)) {
         coun++;
       }
     }
