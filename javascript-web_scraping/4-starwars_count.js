f2197f8142471dddaf645c0c032861add5421b3d
melekmoalla <melekmoalla2468@gmail.com>
2023-04-24 18:33:08 +0100

    test http or https

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,24 +2,14 @@
 const movie = process.argv[2];
 
 const request = require('request');
-let m2;
-request(movie, (error, response, body) => {
+const m1 = movie.split('/');
+const m = `${m1[0]}//${m1[2]}/api/people/18`;
+request(m, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    if (movie === 'https://swapi-api.hbtn.io/api/films') {
-      m2 = 'https://swapi-api.hbtn.io/api/people/18/';
-    } else {
-      m2 = 'http://swapi.co/api/people/18/';
-    }
-    const movie5 = JSON.parse(body).results;
-    let coun = 0;
-    for (let i = 0; i < movie5.length; i++) {
-      const p = movie5[i].characters;
-      if (p.includes(m2)) {
-        coun++;
-      }
-    }
-    console.log(coun);
+    const movie5 = JSON.parse(body);
+    const len = movie5.films;
+    console.log(len.length);
   }
 });
